4bd62c58e78d0b2654581b16a997f461815124b8
Collin Dutter <cdutter@calpoly.edu>
2017-11-01 20:31:44 -0700

    Added basics for genetic algorithm/neural net

--- ball.py
+++ ball.py
@@ -16,3 +16,6 @@ class Ball:
 
     def get_position(self):
         return self.bounds.x, self.bounds.y
+
+    def intersects_paddle(self, cpu_paddle, human_paddle):
+        return (self.bounds.x <= cpu_paddle.bounds.x + cpu_paddle.bounds.width and cpu_paddle.bounds.y <= self.bounds.y <= cpu_paddle.bounds.y + cpu_paddle.bounds.height) or (self.bounds.x + self.bounds.width >= human_paddle.bounds.x and human_paddle.bounds.y <= self.bounds.y <= human_paddle.bounds.y + human_paddle.bounds.height)
